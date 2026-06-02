99f93150658d441a70b314482ca45c4b33a8f6c7
seivarya <seivarya.he@gmail.com>
2026-06-02 17:48:41 +0530

    [refactor]: changes file system, restored theme

---
+++ src/components/mdx/overrides.ts
@@ -0,0 +1,5 @@
+// For basic styling, /styles/mdx.css will do just fine! Using custom component overrides
+// for standard HTML elements (h1-h6, blockquote, etc.) strips away compiler-generated
+// attributes (like heading IDs for anchor links). We keep this object empty for now.
+
+export const mdxOverrides = {};
