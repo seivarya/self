7c28e93005b0d2ea52beaa06f8ff727b03f536d5
seivarya <seivarya.he@gmail.com>
2026-06-03 09:29:39 +0530

    [refactor]: fix

--- src/components/mdx/overrides.ts
+++ src/components/mdx/overrides.ts
@@ -2,4 +2,8 @@
 // for standard HTML elements (h1-h6, blockquote, etc.) strips away compiler-generated
 // attributes (like heading IDs for anchor links). We keep this object empty for now.
 
-export const mdxOverrides = {};
+import Link from "./hyprlink.astro";
+
+export const mdxOverrides = {
+	a: Link
+};
